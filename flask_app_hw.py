d93f067d181058efc85bf135ef898dc20fcb91eb
Tomer Antman <tomerantman@gmail.com>
2021-11-18 13:51:39 +0200

    finished 1st and 2nd task

--- flask_app_hw.py
+++ flask_app_hw.py
@@ -1,17 +1,31 @@
 import sys
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from json import dumps
 from markupsafe import escape
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 @app.route("/")
 def main():
     return render_template("main.html", title="Students")
 
 # list all people
+## see https://camposha.info/python-examples/flask-list-to-jinja2/
 @app.route("/all_students")
 def all():
-    sys.path.insert(0, 'https://github.com/TomerAntman/wis-advanced-python-2021-2022/application')
-    import call_students
-    return (call_students.sorted_list)
+    sys.path.insert(1, '../wis-advanced-python-2021-2022/application')
+    from call_students import call_students
+    student_list = call_students()
+    return render_template("student_list.html", the_list = student_list)
 
+# Link to details after clicking:
+@app.route('/all_students/<student_name>', methods=['GET', 'POST'])
+def profile(student_name):
+    sys.path.insert(1, '../wis-advanced-python-2021-2022/application')
+    from call_students import name_to_pretty_json
+
+    # app.jinja_env.filters['tojson_pretty'] = name_to_pretty_json(student_name)
+    json_to_show = name_to_pretty_json(student_name)
+    return render_template('profile.html', student = json_to_show)
+
+app.run(debug=True)
